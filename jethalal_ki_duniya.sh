a5bc2aa3428dbe88a3318f9da97616e60e38dbc3
Ashish Kavale <ashishkavale@gmail.com>
2025-12-09 17:55:10 +0000

    test

---
+++ jethalal_ki_duniya.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# This is jethalal ki duniya
+<< comment
+writer
+anything
+kuchbhi here will not be exicuted
+comment
+
+name="jetha"
+
+echo "Name is $name"
---
+++ jethya.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+#This is jethaya
+
+<<comment
+any
+hi
+golu
+comment
+
+name="jethya"
+echo "Name is $name, and date is $(date)"
+echo "enter the name:"
+read username
+echo "You entered $username"
+
+
